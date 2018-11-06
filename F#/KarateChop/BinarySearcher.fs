11a2c6daf22c95a9fbe39a9c0b509e61296974aa
Joel Keyser <keyser.joel@gmail.com>
2018-11-05 23:09:42 -0600

    Add F# form of kata.
    Move existing C# implementation into folder.

---
+++ F#/KarateChop/BinarySearcher.fs
@@ -0,0 +1,19 @@
+﻿module BinarySearcher
+
+let rec binarySearch (array : int[]) x first last =
+    match array with
+    | empty when first > last ->
+        -1
+    | _ ->
+        let middle = (first + last) / 2
+        match array.[middle] with
+        | middleIsEqual when middleIsEqual = x ->
+            middle
+        | middleIsGreater when middleIsGreater > x ->
+            binarySearch array x first (middle - 1)
+        | middleIsLesser when middleIsLesser < x ->
+            binarySearch array x (middle + 1) last
+        | _ -> -1
+
+let search (array : int[]) x =
+    binarySearch array x 0 (array.Length - 1)
---
+++ F#/KarateChopTests/BinarySearcherTests.fs
@@ -0,0 +1,52 @@
+﻿module BinarySearcherTests
+
+open Xunit
+
+[<Fact>]
+let ``Searching empty list returns -1`` () =
+    let actual = BinarySearcher.search [||] 0
+    Assert.Equal(-1, actual)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(1, 0)>]
+[<InlineData(2, -1)>]
+let ``Search array of size one`` (target, expected) =
+    let actual = BinarySearcher.search [|1|] target
+    Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(1, 0)>]
+[<InlineData(2, -1)>]
+[<InlineData(3, 1)>]
+[<InlineData(4, -1)>]
+let ``Search array of size two`` (target, expected) =
+    let actual = BinarySearcher.search [|1; 3|] target
+    Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(1, 0)>]
+[<InlineData(2, -1)>]
+[<InlineData(3, 1)>]
+[<InlineData(4, -1)>]
+[<InlineData(5, 2)>]
+[<InlineData(6, -1)>]
+let ``Search array of size three`` (target, expected) =
+    let actual = BinarySearcher.search [|1; 3; 5|] target
+    Assert.Equal(expected, actual)
+
+[<Theory>]
+[<InlineData(0, -1)>]
+[<InlineData(1, 0)>]
+[<InlineData(2, -1)>]
+[<InlineData(3, 1)>]
+[<InlineData(4, -1)>]
+[<InlineData(5, 2)>]
+[<InlineData(6, -1)>]
+[<InlineData(7, 3)>]
+[<InlineData(8, -1)>]
+let ``Search array of size four`` (target, expected) =
+    let actual = BinarySearcher.search [|1; 3; 5; 7|] target
+    Assert.Equal(expected, actual)
